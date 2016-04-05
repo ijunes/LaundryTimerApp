fa8a6b86c50038af06588582dde3c2e324ccf71a
jkang <jkang@eharmony.com>
2016-04-05 14:23:45 -0700

    initial commit

---
+++ wear/src/main/java/com/ijunes/laundrytimer/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package com.ijunes.laundrytimer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.ijunes.laundrytimer.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
