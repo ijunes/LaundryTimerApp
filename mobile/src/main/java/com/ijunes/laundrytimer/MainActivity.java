86f47b9bbd178bcfe1c9e8716df7af3fb23cbcaa
jkang <jkang@eharmony.com>
2016-04-05 14:32:30 -0700

    Add Rx EventBus

--- mobile/src/main/java/com/ijunes/laundrytimer/MainActivity.java
+++ mobile/src/main/java/com/ijunes/laundrytimer/MainActivity.java
@@ -3,10 +3,8 @@ package com.ijunes.laundrytimer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,10 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.hwangjr.rxbus.annotation.Subscribe;
+import com.hwangjr.rxbus.annotation.Tag;
+import com.hwangjr.rxbus.thread.EventThread;
 import com.ijunes.laundrytimer.adapters.SectionsPagerAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,6 +70,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        EventBus.INSTANCE.getBus().register(this);
     }
 
     @Override
@@ -128,4 +131,26 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onDestroy(){
+        EventBus.INSTANCE.getBus().unregister(this);
+        super.onDestroy();
+    }
+
+
+    @Subscribe(
+            thread = EventThread.IO,
+            tags = { @Tag(BusAction.START_WASH) }
+    )
+    public void startWash(long machineId) {
+        // purpose
+    }
+
+    @Subscribe(
+            thread = EventThread.IO,
+            tags = { @Tag(BusAction.START_DRY) }
+    )
+    public void startDry(long machineId) {
+        // purpose
+    }
 }
